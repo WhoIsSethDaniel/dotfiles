cfd95c51c796e35ac6da09854cca52a7003f7f54
Seth Daniel <seth@sethdaniel.org>
2021-10-11 20:49:32 -0500

    .

--- .config/nvim/lua/plugins/telescope-nvim.lua
+++ .config/nvim/lua/plugins/telescope-nvim.lua
@@ -76,4 +76,4 @@ require('telescope').setup {
 }
 
 require('telescope').load_extension 'fzy_native'
--- require('telescope').load_extension 'goldsmith'
+require('telescope').load_extension 'goldsmith'

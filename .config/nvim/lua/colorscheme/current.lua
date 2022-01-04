a5ce7a4fc2e4ab231e05a61be420711c9670e9a6
Seth Daniel <sdaniel@maxmind.com>
2022-01-04 16:29:39 -0600

    .

--- .config/nvim/lua/colorscheme/current.lua
+++ .config/nvim/lua/colorscheme/current.lua
@@ -1,11 +1,29 @@
-require('github-theme').setup {
-  theme_style = 'dark',
-  keyword_style = 'bold',
-  hide_inactive_statusline = false,
-  sidebars = { 'qf' },
-  dark_float = false,
-  -- colors = { bg = '#000000', line_nr = '#008800' }
-  colors = { bg = '#000000' },
-}
+-- require('github-theme').setup {
+--   theme_style = 'dark',
+--   keyword_style = 'bold',
+--   hide_inactive_statusline = false,
+--   sidebars = { 'qf' },
+--   dark_float = false,
+--   -- colors = { bg = '#000000', line_nr = '#008800' }
+--   colors = { bg = '#000000' },
+-- }
+--
+-- vim.cmd [[ colorscheme github_dark ]]
 
-vim.cmd [[ colorscheme github_dark ]]
+require('kanagawa').setup({
+    undercurl = true,           -- enable undercurls
+    commentStyle = "italic",
+    functionStyle = "NONE",
+    keywordStyle = "italic",
+    statementStyle = "bold",
+    typeStyle = "NONE",
+    variablebuiltinStyle = "italic",
+    specialReturn = true,       -- special highlight for the return keyword
+    specialException = true,    -- special highlight for exception handling keywords 
+    transparent = false,        -- do not set background color
+    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+    colors = { bg = '#000000' },
+    overrides = {},
+})
+
+vim.cmd [[ colorscheme kanagawa ]]

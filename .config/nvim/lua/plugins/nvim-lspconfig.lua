4219057d17794ff0933313910fd3b4b1697162b5
Seth Daniel <seth@sethdaniel.org>
2022-01-03 20:28:26 -0600

    .

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,7 +4,7 @@ local function setup_servers()
   local servers = i.get_installed_servers()
 
   for _, server in pairs(servers) do
-    if not require('goldsmith').needed(server) then
+    if not require('goldsmith').needed(server.name) then
       local config = require('lsp').get_config(server.name)
       server:setup(config)
     end

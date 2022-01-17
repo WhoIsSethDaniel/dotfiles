98505d7d91a3daf48a1dca0d9ec7c95a64b31260
Seth Daniel <seth@sethdaniel.org>
2022-01-17 15:30:52 -0600

    .

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,5 +1,6 @@
 -- this must be here since filetype detection now
--- happens prior to init.lua being sourced
+-- happens prior to init.lua being sourced;
+-- see: https://github.com/neovim/neovim/issues/14090#issuecomment-1014888611
 if vim.fn.has 'nvim-0.7' == 1 then
   vim.g.do_filetype_lua = 1
   vim.g.did_load_filetypes = 0

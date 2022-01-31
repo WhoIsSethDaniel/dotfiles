8141550d8c39a48f7865e6eab131fc74e3764496
Seth Daniel <seth@sethdaniel.org>
2022-01-31 15:58:42 -0600

    .

--- .config/nvim/lua/lsp/perlpls.lua
+++ .config/nvim/lua/lsp/perlpls.lua
@@ -6,9 +6,9 @@ return {
       perlcritic = { enabled = true },
     },
   },
-  on_new_config = function(new_config, new_root)
-    if string.match(new_root, 'mm_website') then
-      new_config.settings.perl.perltidyrc = 'perltidyrc'
-    end
-  end,
+  -- on_new_config = function(new_config, new_root)
+  --   if string.match(new_root, 'mm_website') then
+  --     new_config.settings.perl.perltidyrc = 'perltidyrc'
+  --   end
+  -- end,
 }

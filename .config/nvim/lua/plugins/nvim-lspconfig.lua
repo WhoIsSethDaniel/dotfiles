492075c75a1fbdbec6b2c829caaba6e28b64f28b
Seth Daniel <seth@sethdaniel.org>
2022-03-02 13:21:29 -0600

    .

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -9,13 +9,26 @@ local function setup_servers()
       server:setup(config)
     end
   end
-  require('lspconfig').perlpls.setup(require('lsp').get_config 'perlpls')
+  -- require('lspconfig').perlpls.setup(require('lsp').get_config 'perlpls')
+
+  -- Check if the config is already defined (useful when reloading this file)
+  local configs = require 'lspconfig.configs'
+  configs['perlnavigator'] = {
+    default_config = {
+      cmd = { 'node', '/home/seth/src/PerlNavigator/server/out/server.js', '--stdio' },
+      root_dir = function(fname)
+        return require('lspconfig').util.find_git_ancestor(fname)
+      end,
+      filetypes = { 'perl' },
+      settings = {},
+    },
+  }
+  require('lspconfig').perlnavigator.setup {}
 end
 
 setup_servers()
 
 -- require'vim.lsp.log'.set_level(vim.log.levels.TRACE)
--- require'vim.lsp.log'.set_level(vim.log.levels.DEBUG)
-require'vim.lsp.log'.set_level(vim.log.levels.INFO)
-require'vim.lsp.log'.set_format_func(vim.inspect)
-
+require'vim.lsp.log'.set_level(vim.log.levels.DEBUG)
+-- require('vim.lsp.log').set_level(vim.log.levels.INFO)
+require('vim.lsp.log').set_format_func(vim.inspect)

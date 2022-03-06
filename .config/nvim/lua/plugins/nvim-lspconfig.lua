4be926f65ff71ef78b681111074256f3f2bd771f
Seth Daniel <seth@sethdaniel.org>
2022-03-06 13:37:07 -0600

    .

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,15 +1,16 @@
 local i = require 'nvim-lsp-installer'
 
+local disabled = { 'perlnavigator' }
 local function setup_servers()
   local servers = i.get_installed_servers()
 
   for _, server in pairs(servers) do
-    if not require('goldsmith').needed(server.name) then
+    if not vim.tbl_contains(disabled, server.name) and not require('goldsmith').needed(server.name) then
       local config = require('lsp').get_config(server.name)
       server:setup(config)
     end
   end
-  -- require('lspconfig').perlpls.setup(require('lsp').get_config 'perlpls')
+  require('lspconfig').perlpls.setup(require('lsp').get_config 'perlpls')
 end
 
 setup_servers()

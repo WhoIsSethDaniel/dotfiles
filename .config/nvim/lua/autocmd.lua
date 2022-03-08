b2069c2e9cc5ab8e66b5e273d70a117f47c5b848
Seth Daniel <seth@sethdaniel.org>
2022-03-07 21:37:11 -0600

    .

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,71 +1,113 @@
--- open certain things in a vertical split and set options for docs
-local group = 'doc_settings_and_win'
-vim.api.nvim_create_augroup(group, { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  group = group,
-  pattern = { 'help', 'man' },
-  command = 'wincmd L',
-})
-vim.api.nvim_create_autocmd('FileType', {
-  group = group,
-  pattern = { 'lir', 'help', 'man' },
-  callback = function()
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.opt_local.signcolumn = 'no'
-    vim.opt_local.bufhidden = 'wipe'
-  end,
-})
+local terminal_open_setup = function()
+  -- allow window movements
+  vim.api.nvim_set_keymap('t', '<C-w>', '<C-\\><C-n><C-w>', { silent = true, noremap = true })
+  vim.opt_local.signcolumn = 'no'
+  -- vim.opt_local.number = false
+  -- vim.opt_local.relativenumber = false
+  -- vim.api.nvim_command 'startinsert'
+end
 
--- highlighted yank
-group = 'highlight_yank'
-vim.api.nvim_create_augroup(group, { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = group,
-  callback = function()
-    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500, on_visual = true }
-  end,
-})
+-- create any missing intermediate directories for files that do not exist
+local create_missing_dirs = function()
+  local dir = vim.fn.expand '<afile>:h:p'
+  if vim.fn.isdirectory(dir) == 0 then
+    vim.fn.mkdir(dir, 'p')
+    vim.api.nvim_command('saveas! ' .. vim.fn.expand '<afile>')
+  end
+end
 
--- toggleterm takes care of most things terminal related
-group = 'terminal_settings'
-vim.api.nvim_create_augroup(group, { clear = true })
-vim.api.nvim_create_autocmd('TermOpen', {
-  group = group,
-  callback = function()
-    require('functions').terminal_open_setup()
-  end,
-})
+-- find all third-party plugins and rebuild the help tags
+local rebuild_help = function()
+  print 'rebuild all third-party plugin help tags'
+  for _, rtdir in pairs(vim.opt.runtimepath:get()) do
+    local docdir = vim.fn.glob(vim.fn.expand(vim.fn.fnameescape(rtdir .. '/doc')))
+    local tagsfile = vim.fn.glob(docdir .. '/tags')
+    if
+      (#tagsfile > 0 and vim.fn.filewritable(tagsfile) == 1) or (#tagsfile == 0 and vim.fn.filewritable(docdir) == 2)
+    then
+      vim.api.nvim_command('helptags' .. docdir)
+    end
+  end
+end
 
--- create missing directories when opening new files
-group = 'create_missing_dirs'
-vim.api.nvim_create_augroup(group, { clear = true })
-vim.api.nvim_create_autocmd('BufWritePre', {
-  group = group,
-  callback = function()
-    require('functions').create_missing_dirs()
-  end,
-})
+local autocmds = {
+  doc_settings_and_win = {
+    {
+      FileType = {
+        pattern = { 'help', 'man' },
+        command = 'wincmd L',
+      },
+    },
+    {
+      FileType = {
+        pattern = { 'lir', 'help', 'man' },
+        callback = function()
+          vim.opt_local.number = false
+          vim.opt_local.relativenumber = false
+          vim.opt_local.signcolumn = 'no'
+          vim.opt_local.bufhidden = 'wipe'
+        end,
+      },
+    },
+  },
+  highlight_yank = {
+    {
+      TextYankPost = {
+        callback = function()
+          vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500, on_visual = true }
+        end,
+      },
+    },
+  },
+  terminal_settings = {
+    {
+      TermOpen = {
+        callback = function()
+          terminal_open_setup()
+        end,
+      },
+    },
+  },
+  create_missing_dirs = {
+    {
+      BufWritePre = {
+        callback = function()
+          create_missing_dirs()
+        end,
+      },
+    },
+  },
+  vim_on_start = {
+    {
+      VimEnter = {
+        callback = function()
+          rebuild_help()
+          vim.cmd [[ TSUpdate ]]
+        end,
+      },
+    },
+  },
+  neovim_remote = {
+    {
+      FileType = {
+        pattern = { 'gitcommit', 'gitrebase', 'gitconfig' },
+        callback = function()
+          vim.opt_local.bufhidden = 'delete'
+        end,
+      },
+    },
+  },
+}
 
--- re-build help tag files on start;
--- update treesitter parsers
-group = 'vim_on_start'
-vim.api.nvim_create_augroup(group, { clear = true })
-vim.api.nvim_create_autocmd('VimEnter', {
-  group = group,
-  callback = function()
-    require('functions').rebuild_help()
-    vim.cmd [[ TSUpdate ]]
-  end,
-})
-
--- mark git buffers as wipe/delete on close
-group = 'neovim_remote'
-vim.api.nvim_create_augroup(group, { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  group = group,
-  pattern = { 'gitcommit', 'gitrebase', 'gitconfig' },
-  callback = function()
-    vim.opt_local.bufhidden = 'delete'
-  end,
-})
+-- setup all autocmds
+do
+  for group, grpdef in pairs(autocmds) do
+    vim.api.nvim_create_augroup(group, { clear = true })
+    for _, events in ipairs(grpdef) do
+      -- this never has more than one event
+      for event, eventdef in pairs(events) do
+        vim.api.nvim_create_autocmd(event, eventdef)
+      end
+    end
+  end
+end

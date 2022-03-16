47847f9db9065a354da1aea6b9669fa428144230
Seth Daniel <seth@sethdaniel.org>
2022-03-16 21:12:28 +0000

    .

--- .config/nvim/lua/plugins/project-nvim.lua
+++ .config/nvim/lua/plugins/project-nvim.lua
@@ -1,4 +1,5 @@
-require'project_nvim'.setup({
-  patterns = { "go.work", ".git", "go.mod", "Makefile" },
-  silent_chdir = true
-})
+require('project_nvim').setup {
+  patterns = { 'go.work', '.git', 'go.mod', 'Makefile' },
+  silent_chdir = true,
+  ignore_lsp = { 'null-ls' },
+}

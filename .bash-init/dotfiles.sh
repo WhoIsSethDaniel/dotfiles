6f6d82be4f5d952daf4344f21d21fd2ecd80f975
Seth Daniel <seth@sethdaniel.org>
2022-03-17 18:26:17 -0500

    .

--- .bash-init/dotfiles.sh
+++ .bash-init/dotfiles.sh
@@ -4,17 +4,17 @@ GIT_HOME=$HOME/.config/dotfiles/home
 
 mkdir -p "$GIT_HOME"
 
-function check () {
-  git --git-dir="$GIT_HOME" --work-tree="$HOME" "$@"
+function check() {
+    git --git-dir="$GIT_HOME" --work-tree="$HOME" "$@"
 }
 
-function gh-cd () {
-  cd "$HOME/.config/dotfiles"
+function gh-cd() {
+    cd "$HOME/.config/dotfiles"
 }
 
 # check config status.showUntrackedFiles no
 check config core.excludesfile ~/.config/dotfiles/global/git/ignore
 
 for DIR in "$HOME/.config/dotfiles/"*/bin; do
-  set_post_path_var PATH "$DIR"
+    set_post_path_var PATH "$DIR"
 done

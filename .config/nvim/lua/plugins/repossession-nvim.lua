f3a43ece2ecdd31705f4b5a57f3a0e9a7f51adc7
Seth Daniel <seth@sethdaniel.org>
2022-03-23 16:19:16 -0500

    .

--- .config/nvim/lua/plugins/repossession-nvim.lua
+++ .config/nvim/lua/plugins/repossession-nvim.lua
@@ -1,33 +1,22 @@
 require('repossession').setup {
   auto = true,
   continuous_save = true,
-  blacklist_dirs = vim.tbl_map(
-    function(d)
-      return vim.fn.expand(d)
-    end,
-    vim.tbl_flatten {
-      '~',
-      '~/bin',
-      '~/.bash-init',
-      '~/tmp',
-      '~/.config/nvim',
-      '~/.config/nvim/lua',
-      '~/src/site',
-      vim.tbl_filter(function(f)
-        if string.match(f, 'dotfiles/home/') then
-          return false
-        end
-        return vim.fn.isdirectory(f) > 0 and true or false
-      end, vim.fn.glob('~/.config/dotfiles/**', false, true)),
-      vim.tbl_filter(function(f)
-        return vim.fn.isdirectory(f) > 0 and true or false
-      end, vim.fn.glob('~/.config/nvim/lua/**', false, true)),
-      vim.tbl_filter(function(f)
-        return vim.fn.isdirectory(f) > 0 and true or false
-      end, vim.fn.glob('~/src/site/**', false, true)),
-      vim.tbl_filter(function(f)
-        return vim.fn.isdirectory(f) > 0 and true or false
-      end, vim.fn.glob('~/.local/share/nvim/**', false, true)),
-    }
-  ),
+  blacklist_dirs = vim.tbl_flatten {
+    '~',
+    '~/bin',
+    '~/.bash-init',
+    '~/tmp',
+    '~/.config/nvim',
+    '~/.config/nvim/lua',
+    '~/src/site',
+    '~/.config/nvim/lua/**',
+    '~/src/site/**',
+    '~/.local/share/nvim/**',
+    vim.tbl_filter(function(f)
+      if string.match(f, 'dotfiles/home/') then
+        return false
+      end
+      return vim.fn.isdirectory(f) > 0 and true or false
+    end, vim.fn.glob('~/.config/dotfiles/**', false, true)),
+  },
 }

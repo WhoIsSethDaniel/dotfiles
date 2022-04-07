3008e4cca05694d6b27428b71f5135b3f799f44f
Seth Daniel <seth@sethdaniel.org>
2022-04-07 14:49:43 +0000

    .

--- .config/nvim/lua/plugins/toggleterm-nvim.lua
+++ .config/nvim/lua/plugins/toggleterm-nvim.lua
@@ -1,4 +1,4 @@
-require("toggleterm").setup{
+require('toggleterm').setup {
   -- size can be a number or function which is passed the current terminal
   size = 120,
   open_mapping = [[<C-t>]],
@@ -7,7 +7,7 @@ require("toggleterm").setup{
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true,
-  shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
   insert_mappings = false, -- whether or not the open mapping applies in insert mode
   persist_size = true,
@@ -26,8 +26,8 @@ require("toggleterm").setup{
     height = 20,
     winblend = 3,
     highlights = {
-      border = "Normal",
-      background = "Normal",
-    }
-  }
+      border = 'Normal',
+      background = 'Normal',
+    },
+  },
 }

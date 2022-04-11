8ab8c9b0309004babe29a8fdfee00244bcdc6edb
Seth Daniel <seth@sethdaniel.org>
2022-04-11 13:27:14 -0500

    .

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 --  https://github.com/nvim-treesitter/nvim-treesitter
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'all',
-  ignore_install = { 'cooklang' },
+  -- ignore_install = { 'cooklang' },
   highlight = {
     enable = true,
   },

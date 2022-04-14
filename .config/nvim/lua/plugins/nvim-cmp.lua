24e11f9223e7585b02c93f687be09221ba4fe733
Seth Daniel <seth@sethdaniel.org>
2022-04-14 09:14:10 -0500

    .

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -28,11 +28,7 @@ cmp.setup {
       vim.fn['vsnip#anonymous'](args.body)
     end,
   },
-  mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
+  mapping = cmp.mapping.preset.insert {
     ['<CR>'] = cmp.mapping.confirm { select = false },
   },
   sources = {

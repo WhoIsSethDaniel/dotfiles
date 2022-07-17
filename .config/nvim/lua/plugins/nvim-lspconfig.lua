d1e59ad2964f5abde4cc644b1ec7e33aa68e137e
Seth Daniel <seth@sethdaniel.org>
2022-07-16 20:02:20 -0500

    .

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,8 +2,36 @@ require('mason').setup {}
 local m = require 'mason-lspconfig'
 
 m.setup {
-  ensure_installed = { 'bashls', 'sumneko_lua', 'perlnavigator', 'vimls', 'gopls' },
-  automatic_installation = true,
+  -- ensure_installed = { 'bashls', 'sumneko_lua', 'perlnavigator', 'vimls', 'gopls' },
+  automatic_installation = false,
+}
+
+require('mason-tool-installer').setup {
+  ensure_installed = {
+    'bash-language-server',
+    'lua-language-server',
+    'perlnavigator',
+    'vim-language-server',
+    'gopls',
+    'stylua',
+    'shellcheck',
+    'editorconfig-checker',
+    'gofumpt',
+    'golangci-lint',
+    'golines',
+    'gomodifytags',
+    'gotests',
+    'impl',
+    'json-to-struct',
+    'luacheck',
+    'misspell',
+    'revive',
+    'shellcheck',
+    'shfmt',
+    'staticcheck',
+    'vint',
+  },
+  auto_update = true,
 }
 
 local disabled = {}

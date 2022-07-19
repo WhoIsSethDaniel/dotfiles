4c1100d109c9a2b3f794a3d0f09ae73aa4981079
Seth Daniel <seth@sethdaniel.org>
2022-07-19 13:18:24 -0500

    .

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -19,6 +19,7 @@ null.register {
 return {
   sources = {
     fmt.stylua,
+    fmt.prettier.with { filetypes = { 'markdown' } },
     diag.misspell.with { disabled_filetypes = { 'man' } },
     fmt.shfmt.with { args = { '-i=4', '-ci', '-s', '-bn' } },
     diag.luacheck.with {

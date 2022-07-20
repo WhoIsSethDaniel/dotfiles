840196bee7c3803cddbe66786c03ae7685321e46
Seth Daniel <seth@sethdaniel.org>
2022-07-19 20:39:11 -0500

    .

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,22 @@
 require('mason').setup {
   log_level = vim.log.levels.DEBUG,
 }
+
 local m = require 'mason-lspconfig'
 
-m.setup {
-  -- ensure_installed = { 'bashls', 'sumneko_lua', 'perlnavigator', 'vimls', 'gopls' },
-  automatic_installation = false,
+local disabled = { 'perlpls' }
+-- local disabled = { 'perlnavigator' }
+
+m.setup_handlers {
+  function(server)
+    if not vim.tbl_contains(disabled, server) and not require('goldsmith').needed(server) then
+      local config = require('lsp').get_config(server)
+      require('lspconfig')[server].setup(config)
+    end
+  end,
 }
+
+m.setup {}
 
 require('mason-tool-installer').setup {
   ensure_installed = {
@@ -35,20 +45,6 @@ require('mason-tool-installer').setup {
   },
   auto_update = true,
 }
-
-local disabled = {}
--- local disabled = { 'perlnavigator' }
-
-m.setup_handlers {
-  function(server)
-    if not vim.tbl_contains(disabled, server) and not require('goldsmith').needed(server) then
-      local config = require('lsp').get_config(server)
-      require('lspconfig')[server].setup(config)
-    end
-  end,
-}
-
--- require('lspconfig').perlpls.setup(require('lsp').get_config 'perlpls')
 
 -- require'vim.lsp.log'.set_level(vim.log.levels.TRACE)
 require('vim.lsp.log').set_level(vim.log.levels.DEBUG)

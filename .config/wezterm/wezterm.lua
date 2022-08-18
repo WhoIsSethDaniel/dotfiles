d6f791a596a7eb9829ba4c99b9f4b89b1639bd44
Seth Daniel <seth@sethdaniel.org>
2022-08-17 20:02:40 -0500

    .

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+return {
+  font_size = 18.0,
+  font = wezterm.font 'DejaVuSansMono Nerd Font',
+  color_scheme = 'kanagawabones',
+  default_prog = { '/bin/bash' },
+}

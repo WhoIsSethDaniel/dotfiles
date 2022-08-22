36d2f7de533b060bb725bc06fa61a688dc307b70
Seth Daniel <seth@sethdaniel.org>
2022-08-22 10:59:15 -0500

    .

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -33,7 +33,7 @@ return {
     selection_bg = '#2d4f67',
 
     scrollbar_thumb = '#16161d',
-    split = '#16161d',
+    split = '#36363d',
 
     ansi = { '#090618', '#c34043', '#76946a', '#c0a36e', '#7e9cd8', '#957fb8', '#6a9589', '#c8c093' },
     brights = { '#727169', '#e82424', '#98bb6c', '#e6c384', '#7fb4ca', '#938aa9', '#7aa89f', '#dcd7ba' },

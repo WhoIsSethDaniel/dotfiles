bb331ff9c48a5d71f66227e8e7e9afc6ca80d7ec
Seth Daniel <seth@sethdaniel.org>
2022-08-26 14:34:53 -0500

    .

--- .bash-init/x.sh
+++ .bash-init/x.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ "$WEZTERM_EXECUTABLE" == "" ]; then
+if [ "$WEZTERM_EXECUTABLE" == "" ] && [ "$ALACRITTY_SOCKET" == "" ]; then
     # need the 'vte' for any VTE terminal. Terminator sets it to xterm-256color.
     set_export_var TERM vte-256color
-else
-    set_export_var TERM wezterm
 fi

9880f913801733407c90c81d5346e9c3a34e2b43
Seth Daniel <seth@sethdaniel.org>
2022-09-12 14:47:53 -0500

    .

--- .config/nvim/lua/plugins/lualine-nvim.lua
+++ .config/nvim/lua/plugins/lualine-nvim.lua
@@ -10,19 +10,32 @@ local theme = require 'lualine.themes.kanagawa'
 
 require('lualine').setup {
   options = {
-    theme = theme,
-    section_separators = '',
-    component_separators = '',
     icons_enabled = true,
+    theme = theme,
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = { 'toggleterm' },
+    },
+    ignore_focus = {},
     always_divide_middle = true,
     globalstatus = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
   },
   sections = {
     lualine_a = { 'mode' },
     lualine_b = {
       'hostname',
       'branch',
-      { 'diagnostics', update_in_insert = true, always_visible = true, cond = vim.lsp.buf.server_ready },
+      'diagnostics',
+      update_in_insert = true,
+      always_visible = true,
+      cond = vim.lsp.buf.server_ready,
     },
     lualine_c = {
       'diff',
@@ -32,7 +45,22 @@ require('lualine').setup {
     },
     lualine_x = { 'lsp_progress', 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
-    lualine_z = { location },
+    lualine_z = { 'location' },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  winbar = {
+    lualine_z = { 'filename' },
+  },
+  inactive_winbar = {
+    lualine_z = { 'filename' },
   },
   extensions = { 'fugitive' },
 }

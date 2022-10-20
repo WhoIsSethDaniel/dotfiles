cfbcf316d34eb1355ceb310c5841b2d3f7fedc26
Seth Daniel <seth@sethdaniel.org>
2022-10-20 14:12:29 -0500

    .

--- .config/nvim/lua/plugins/noice-nvim.lua
+++ .config/nvim/lua/plugins/noice-nvim.lua
@@ -1,1 +1,23 @@
-require('noice').setup()
+require('noice').setup {
+  cmdline = {
+    enabled = true,
+    view = 'cmdline_popup',
+  },
+  messages = {
+    enabled = true,
+    view = 'mini',
+    view_error = 'mini',
+    view_warn = 'mini',
+    view_history = 'split',
+    view_search = 'virtualtext',
+  },
+  notify = {
+    enabled = true,
+    view = 'mini',
+  },
+  views = {
+    mini = {
+      timeout = 4000,
+    },
+  },
+}

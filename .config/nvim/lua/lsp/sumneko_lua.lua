37e7b0af5c1aedf1f4f9e5aace3ab762504cfe01
Seth Daniel <seth@sethdaniel.org>
2022-11-16 11:18:25 -0600

    .

--- .config/nvim/lua/lsp/sumneko_lua.lua
+++ .config/nvim/lua/lsp/sumneko_lua.lua
@@ -1,41 +1,25 @@
-return {}
--- local lspconfig = require 'lspconfig'
---
--- local path = {}
--- table.insert(path, '?.lua')
--- table.insert(path, '?/init.lua')
---
--- return {
---   on_new_config = lspconfig.util.add_hook_after(lspconfig.util.default_config.on_new_config, function(config, root_dir)
---     local lib = vim.tbl_deep_extend('force', {}, config.settings.Lua.workspace.library)
---     lib[vim.loop.fs_realpath(root_dir) .. '/lua'] = nil
---     lib[vim.loop.fs_realpath(root_dir)] = nil
---     config.settings.Lua.workspace.library = lib
---     return config
---   end),
---   settings = {
---     Lua = {
---       format = {
---         enable = false,
---       },
---       runtime = {
---         version = 'LuaJIT',
---         path = path,
---       },
---       diagnostics = {
---         globals = { 'vim' },
---         -- neededFileStatus = {
---         --   ['codestyle-check'] = 'Any',
---         -- },
---       },
---       hint = { enable = true },
---       workspace = {
---         library = vim.api.nvim_get_runtime_file('', true),
---         checkThirdParty = false,
---         maxPreload = 4000,
---         preloadFileSize = 150,
---       },
---       telemetry = { enable = false },
---     },
---   },
--- }
+return {
+  settings = {
+    Lua = {
+      format = {
+        enable = false,
+      },
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' },
+        -- neededFileStatus = {
+        --   ['codestyle-check'] = 'Any',
+        -- },
+      },
+      hint = { enable = true },
+      workspace = {
+        checkThirdParty = false,
+        maxPreload = 4000,
+        preloadFileSize = 150,
+      },
+      telemetry = { enable = false },
+    },
+  },
+}

43b9c7ef6d4987e1786be7c3f44106416b921608
Seth Daniel <seth@sethdaniel.org>
2023-02-24 14:55:41 -0600

    .

--- .config/nvim/lua/plugins/oil-nvim.lua
+++ .config/nvim/lua/plugins/oil-nvim.lua
@@ -20,7 +20,7 @@ require('oil').setup {
     spell = false,
     list = false,
     conceallevel = 3,
-    concealcursor = 'n',
+    concealcursor = 'nvic',
   },
   -- Restore window options to previous values when leaving an oil buffer
   restore_win_options = true,

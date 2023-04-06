dfbd99e15636ede56c1a7f568750b425e325bfee
Seth Daniel <seth@sethdaniel.org>
2023-04-06 20:08:30 +0000

    .

--- .config/nvim/lua/plugins/wiki-vim.lua
+++ .config/nvim/lua/plugins/wiki-vim.lua
@@ -1,11 +1,10 @@
 vim.g.wiki_root = '/home/seth/.config/dotfiles/main/notes'
-vim.g.wiki_link_extension = 'wiki'
-vim.g.wiki_file_handle = '.wiki'
-vim.g.wiki_link_toggle_on_follow = 'wiki'
+vim.g.wiki_filetypes = { 'wiki' }
+vim.g.wiki_link_extension = '.wiki'
 vim.api.nvim_exec(
   [[
     augroup init_wiki
-    autocmd BufRead,BufNewFile *.wiki set filetype=markdown
+    autocmd BufRead,BufNewFile *.wiki set filetype=wiki
     augroup END
 ]],
   false

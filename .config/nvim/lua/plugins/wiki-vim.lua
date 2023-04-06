3d6ed4299b41740d2c4cd3587352f06e7fda8dfe
Seth Daniel <seth@sethdaniel.org>
2023-04-06 15:00:14 -0500

    .

--- .config/nvim/lua/plugins/wiki-vim.lua
+++ .config/nvim/lua/plugins/wiki-vim.lua
@@ -1,4 +1,7 @@
 vim.g.wiki_root = '/home/seth/.config/dotfiles/main/notes'
+vim.g.wiki_link_extension = 'wiki'
+vim.g.wiki_file_handle = '.wiki'
+vim.g.wiki_link_toggle_on_follow = 'wiki'
 vim.api.nvim_exec(
   [[
     augroup init_wiki

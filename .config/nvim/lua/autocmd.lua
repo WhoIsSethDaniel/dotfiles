b6c49e67fcf02650cb79c52bf7e93205069fb7e5
Seth Daniel <seth@sethdaniel.org>
2023-05-19 17:00:43 -0500

    .

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -31,6 +31,19 @@ local rebuild_help = function()
 end
 
 local autocmds = {
+  fix_telescope_insert = {
+    {
+      -- https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1510001730
+      -- workaround for: https://github.com/nvim-telescope/telescope.nvim/issues/2501
+      WinLeave = {
+        callback = function()
+          if vim.bo.ft == 'TelescopePrompt' and vim.fn.mode() == 'i' then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'i', false)
+          end
+        end,
+      },
+    },
+  },
   set_cd = {
     {
       FileType = {

4e293296a3e9f36e5c9765b00f36334e054afcfb
Seth Daniel <seth@sethdaniel.org>
2023-07-11 19:33:27 -0500

    .

--- .config/nvim/lua/lsp/formatter.lua
+++ .config/nvim/lua/lsp/formatter.lua
@@ -1,5 +1,6 @@
 -- https://github.com/mhartington/formatter.nvim
 local util = require 'formatter.util'
+local fdef = require 'formatter.defaults'
 local ft = require 'formatter.filetypes'
 
 vim.api.nvim_create_augroup('Formatter', { clear = true })
@@ -10,20 +11,16 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 })
 
 local function prettier()
-  local d = require('formatter.defaults').prettier
+  local f = fdef.prettier()
   local m = string.match(util.get_current_buffer_file_path(), '^(.*/work)')
   if m then
-    return vim.tbl_extend(
-      'force',
-      d(),
-      { stdin = false, args = { '--config', string.format('%s/mm_website/.prettierrc.non-js.cjs', m) } }
-    )
-  else
-    return d()
+    table.insert(f.args, 1, string.format('%s/mm_website/.prettierrc.non-js.cjs', m))
+    table.insert(f.args, 1, '--config')
+    return f
   end
+  return f
 end
 
--- TODO: prettier not yet working
 return {
   logging = true,
   log_level = vim.log.levels.ERROR,

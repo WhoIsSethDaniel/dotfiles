6fb7bfbc751521b47871b7efc5cefb062f5fce64
Seth Daniel <seth@sethdaniel.org>
2023-07-16 19:45:11 -0500

    .

--- .config/nvim/lua/plugins/focus-nvim.lua
+++ .config/nvim/lua/plugins/focus-nvim.lua
@@ -9,34 +9,34 @@
 --   excluded_filetypes = { 'DressingInput', 'TelescopePrompt' },
 -- }
 
-require("focus").setup({
-    enable = true, -- Enable module
-    commands = true, -- Create Focus commands
-    autoresize = {
-        enable = false, -- Enable or disable auto-resizing of splits
-        width = 0, -- Force width for the focused window
-        height = 0, -- Force height for the focused window
-        minwidth = 0, -- Force minimum width for the unfocused window
-        minheight = 0, -- Force minimum height for the unfocused window
-        height_quickfix = 10, -- Set the height of quickfix panel
-    },
-    split = {
-        bufnew = true, -- Create blank buffer for new split windows
-        tmux = false, -- Create tmux splits instead of neovim splits
-    },
-    ui = {
-        number = false, -- Display line numbers in the focussed window only
-        relativenumber = false, -- Display relative line numbers in the focussed window only
-        hybridnumber = false, -- Display hybrid line numbers in the focussed window only
-        absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+require('focus').setup {
+  enable = true, -- Enable module
+  commands = true, -- Create Focus commands
+  autoresize = {
+    enable = false, -- Enable or disable auto-resizing of splits
+    width = 0, -- Force width for the focused window
+    height = 0, -- Force height for the focused window
+    minwidth = 0, -- Force minimum width for the unfocused window
+    minheight = 0, -- Force minimum height for the unfocused window
+    height_quickfix = 10, -- Set the height of quickfix panel
+  },
+  split = {
+    bufnew = true, -- Create blank buffer for new split windows
+    tmux = false, -- Create tmux splits instead of neovim splits
+  },
+  ui = {
+    number = false, -- Display line numbers in the focussed window only
+    relativenumber = false, -- Display relative line numbers in the focussed window only
+    hybridnumber = false, -- Display hybrid line numbers in the focussed window only
+    absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
 
-        cursorline = true, -- Display a cursorline in the focussed window only
-        cursorcolumn = false, -- Display cursorcolumn in the focussed window only
-        colorcolumn = {
-            enable = false, -- Display colorcolumn in the foccused window only
-            list = '+1', -- Set the comma-saperated list for the colorcolumn
-        },
-        signcolumn = true, -- Display signcolumn in the focussed window only
-        winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
-    }
-})
+    cursorline = false, -- Display a cursorline in the focussed window only
+    cursorcolumn = false, -- Display cursorcolumn in the focussed window only
+    colorcolumn = {
+      enable = false, -- Display colorcolumn in the foccused window only
+      list = '+1', -- Set the comma-saperated list for the colorcolumn
+    },
+    signcolumn = false, -- Display signcolumn in the focussed window only
+    winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+  },
+}

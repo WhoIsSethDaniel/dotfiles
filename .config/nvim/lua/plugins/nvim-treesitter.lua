4c7e8fd4eecf9985a9259d0f0abbea1d9969f806
Seth Daniel <seth@sethdaniel.org>
2023-08-01 18:52:07 +0000

    .

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,6 @@
 --  https://github.com/nvim-treesitter/nvim-treesitter
 vim.api.nvim_create_autocmd('BufRead', {
+  group = vim.api.nvim_create_augroup('StartupBufRead', { clear = true }),
   callback = function()
     ---@diagnostic disable-next-line:missing-fields
     require('nvim-treesitter.configs').setup {

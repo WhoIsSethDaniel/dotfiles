c1c7f8b8fd0ea55864773069602d7c7e4d4a1956
Seth Daniel <seth@sethdaniel.org>
2023-08-17 21:00:30 -0500

    .

--- .config/nvim/lua/lsp/lint.lua
+++ .config/nvim/lua/lsp/lint.lua
@@ -52,6 +52,32 @@ table.insert(yl.args, 1, function()
   end
 end)
 
+-- l.linters.perlimports = function()
+--   local filename = vim.api.nvim_buf_get_name(0)
+--   return {
+--     cmd = 'perlimports',
+--     stdin = true,
+--     args = { '--lint', '--json', '--read-stdin', '--filename', filename },
+--     stream = 'stderr',
+--     parser = function(output)
+--       local result = vim.fn.split(output, '\n')
+--       local diagnostics = {}
+--       for _, message in ipairs(result) do
+--         local decoded = vim.json.decode(message)
+--         table.insert(diagnostics, {
+--           lnum = decoded.location.start.line - 1,
+--           col = decoded.location.start.column - 1,
+--           end_lnum = decoded.location['end'].line - 1,
+--           end_col = decoded.location['end'].column - 1,
+--           severity = vim.diagnostic.severity.INFO,
+--           message = decoded.reason,
+--         })
+--       end
+--       return diagnostics
+--     end,
+--   }
+-- end
+
 l.linters_by_ft = {
   go = { 'golangcilint' },
   lua = { 'selene' },
@@ -59,6 +85,7 @@ l.linters_by_ft = {
   sql = { 'sqlfluff' },
   vim = { 'vint' },
   yaml = { 'yamllint' },
+  -- perl = { 'perlimports' },
 }
 
 return {}

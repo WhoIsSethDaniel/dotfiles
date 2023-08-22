9e09e23589d347d9b8bb4d850070f9b01ef19ea2
Seth Daniel <seth@sethdaniel.org>
2023-08-22 15:48:48 -0500

    .

--- .config/nvim/lua/lsp/lua_ls.lua
+++ .config/nvim/lua/lsp/lua_ls.lua
@@ -1,4 +1,17 @@
+-- local has_luarc = function()
+--   return vim.uv.fs_stat '.luarc.json' or vim.uv.fs_stat '.luarc.jsonc'
+-- end
+
 return {
+  -- on_init = function(client)
+  --   if not has_luarc() then
+  --     local config = client.config
+  --     config.settings =
+  --       vim.tbl_deep_extend('force', config.settings, { Lua = { workspace = { library = { vim.env.VIMRUNTIME } } } })
+  --     print(vim.inspect(config.settings))
+  --     -- client.notify('workspace/didChangeConfiguration', { settings = config.settings })
+  --   end
+  -- end,
   settings = {
     Lua = {
       format = {
@@ -7,17 +20,14 @@ return {
       runtime = {
         version = 'LuaJIT',
       },
-      diagnostics = {
-        globals = { 'vim' },
-        -- neededFileStatus = {
-        --   ['codestyle-check'] = 'Any',
-        -- },
-      },
       hint = { enable = true },
       workspace = {
+        library = {
+          vim.env.VIMRUNTIME,
+        },
         checkThirdParty = false,
-        maxPreload = 4000,
-        preloadFileSize = 150,
+        -- maxPreload = 4000,
+        -- preloadFileSize = 150,
       },
       telemetry = { enable = false },
     },

f380089e22e81764316d18988e2da00c356bf9a1
Seth Daniel <seth@sethdaniel.org>
2023-09-15 14:49:27 -0500

    .

--- .config/nvim/lua/plugins/lualine-nvim.lua
+++ .config/nvim/lua/plugins/lualine-nvim.lua
@@ -11,8 +11,6 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = { 'toggleterm', 'help', 'man' },

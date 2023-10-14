0f28bcb7a19beb33cd4b7cfc5cdf00c3e735a507
Seth Daniel <seth@sethdaniel.org>
2023-10-13 19:00:18 -0500

    .

---
+++ .config/nvim/lua/plugins/bigfile-nvim.lua
@@ -0,0 +1,15 @@
+-- https://github.com/LunarVim/bigfile.nvim
+require('bigfile').setup {
+  filesize = 1,
+  -- pattern = { "*" },
+  -- features = {
+  --   "indent_blankline",
+  --   "illuminate",
+  --   "lsp",
+  --   "treesitter",
+  --   "syntax",
+  --   "matchparen",
+  --   "vimopts",
+  --   "filetype",
+  -- },
+}

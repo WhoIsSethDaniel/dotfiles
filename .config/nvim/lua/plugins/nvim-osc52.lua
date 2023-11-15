340b09742a586378aba69170fd4b66408ec54501
Seth Daniel <seth@sethdaniel.org>
2023-11-15 16:46:33 -0600

    .

---
+++ .config/nvim/lua/plugins/nvim-osc52.lua
@@ -0,0 +1,6 @@
+require('osc52').setup {
+  max_length = 0, -- Maximum length of selection (0 for no limit)
+  silent = false, -- Disable message on successful copy
+  trim = false, -- Trim surrounding whitespaces before copy
+  tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
+}

644b24db3791f582be89f23003a8b8db991d9c54
Seth Daniel <seth@sethdaniel.org>
2023-11-14 19:41:58 -0600

    .

--- .config/nvim/lua/plugins/git-conflict-nvim.lua
+++ .config/nvim/lua/plugins/git-conflict-nvim.lua
@@ -1,6 +1,6 @@
 -- https://github.com/akinsho/git-conflict.nvim
 require('git-conflict').setup {
-  default_mappings = true, -- disable buffer local mapping created by this plugin
+  default_mappings = false, -- disable buffer local mapping created by this plugin
   default_commands = true, -- disable commands created by this plugin
   disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
   list_opener = 'copen', -- command or function to open the conflicts list
@@ -10,3 +10,11 @@ require('git-conflict').setup {
   },
   debug = false,
 }
+
+vim.keymap.set('n', 'co', '<Plug>(git-conflict-ours)')
+vim.keymap.set('n', 'ct', '<Plug>(git-conflict-theirs)')
+vim.keymap.set('n', 'cb', '<Plug>(git-conflict-both)')
+vim.keymap.set('n', 'c0', '<Plug>(git-conflict-none)')
+-- try [n & ]n instead
+-- vim.keymap.set('n', ']x', '<Plug>(git-conflict-prev-conflict)')
+-- vim.keymap.set('n', '[x', '<Plug>(git-conflict-next-conflict)')

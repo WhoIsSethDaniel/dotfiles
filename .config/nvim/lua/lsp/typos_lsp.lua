c8c02ee11a646a2412e88dba49e6e5798109fe03
Seth Daniel <seth@sethdaniel.org>
2023-11-29 14:15:16 -0600

    .

--- .config/nvim/lua/lsp/typos_lsp.lua
+++ .config/nvim/lua/lsp/typos_lsp.lua
@@ -1,5 +1,6 @@
 -- https://github.com/tekumara/typos-vscode
 return {
+  filetypes = { 'go', 'lua', 'markdown', 'sql', 'vim', 'yaml', 'gohtmltmpl', 'json', 'sh', 'toml' },
   settings = {
     typos = {
       -- unclear if this is being used

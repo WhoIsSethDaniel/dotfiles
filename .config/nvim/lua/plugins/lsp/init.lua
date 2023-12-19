0e5c7058e1ab1941c6489248b32a01166bb2d44a
Seth Daniel <seth@sethdaniel.org>
2023-12-19 14:55:30 -0600

    .

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -36,12 +36,6 @@ vim.api.nvim_create_autocmd({ 'LspAttach' }, {
       return
     end
     local bufnr = args.buf
-    if_has_do('lsp-format', function(m)
-      m.on_attach(client)
-    end)
-    if_has_do('lsp_signature', function(m)
-      m.on_attach({}, bufnr)
-    end)
     if client.server_capabilities.documentSymbolProvider then
       if_has_do('nvim-navic', function(m)
         m.attach(client, bufnr)
@@ -69,9 +63,6 @@ vim.api.nvim_create_autocmd({ 'LspAttach' }, {
     map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    if_has_do('lsp-format', function(_)
-      map('n', '<leader>cf', "<cmd>lua require'lsp-format'.format({})<CR>", opts)
-    end)
     if_has_do('conform', function(_)
       map('n', '<leader>cf', "<cmd>lua require'conform'.format()<CR>", opts)
     end)
@@ -111,32 +102,6 @@ function M.setup()
   if_has_do('toggle_lsp_diagnostics', function(m)
     m.init(vim.diagnostic.config())
     vim.api.nvim_set_keymap('n', '<leader>td', '<Plug>(toggle-lsp-diag)', { silent = true })
-  end)
-
-  if_has_do('neodev', function(m)
-    m.setup {
-      library = {
-        enabled = true, -- when not enabled, neodev will not change any settings to the LSP server
-        -- these settings will be used for your Neovim config directory
-        runtime = true, -- runtime path
-        types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-        plugins = true, -- installed opt or start plugins in packpath
-        -- you can also specify the list of plugins to make available as a workspace library
-        -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
-      },
-      setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
-      -- for your Neovim config directory, the config.library settings will be used as is
-      -- for plugin directories (root_dirs having a /lua directory), config.library.plugins will be disabled
-      -- for any other directory, config.library.enabled will be set to false
-      override = function(_, _) end,
-      -- With lspconfig, Neodev will automatically setup your lua-language-server
-      -- If you disable this, then you have to set {before_init=require("neodev.lsp").before_init}
-      -- in your lsp start options
-      lspconfig = true,
-      -- much faster, but needs a recent built of lua-language-server
-      -- needs lua-language-server >= 3.6.0
-      pathStrict = true,
-    }
   end)
 
   if_has_do('mason', function(mason)
@@ -203,22 +168,6 @@ function M.setup()
         -- start_delay = 1000,
       }
     end)
-  end)
-
-  if_has_do('lsp_signature', function(m)
-    m.setup {
-      bind = true,
-      wrap = true,
-      handler_opts = {
-        border = 'rounded',
-      },
-    }
-  end)
-
-  if_has_do('lsp-format', function(m)
-    m.setup {
-      -- perl = { exclude = { 'perlnavigator' } },
-    }
   end)
 
   if_has_do('nvim-navic', function(m)

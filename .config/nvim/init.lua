e3191173f9dcc161e197420c4460bd87ce25ec8c
Seth Daniel <seth@sethdaniel.org>
2024-02-05 10:58:39 -0600

    .

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -238,6 +238,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
 vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
 vim.g.loaded_gzip = 1
 -- vim.g.loaded_man = 1
 -- matchup wants to load matchparen
@@ -245,6 +246,7 @@ vim.g.loaded_gzip = 1
 vim.g.loaded_remote_plugins = 1
 vim.g.loaded_shada_plugin = 1
 vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_tar = 1
 vim.g.loaded_tarPlugin = 1
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_tutor_mode_plugin = 1

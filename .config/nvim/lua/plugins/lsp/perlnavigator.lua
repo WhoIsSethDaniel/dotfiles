d431e3a34d6e170b789a5bc71b22290e6978d436
Seth Daniel <seth@sethdaniel.org>
2024-02-09 19:36:36 +0000

    .

--- .config/nvim/lua/plugins/lsp/perlnavigator.lua
+++ .config/nvim/lua/plugins/lsp/perlnavigator.lua
@@ -9,6 +9,7 @@ return {
       enableProgress = true,
       perlcriticProfile = '$workspaceFolder/.perlcriticrc',
       perltidyProfile = '$workspaceFolder/.perltidyallrc',
+      -- logging = true,
     },
   },
   on_new_config = function(new_config, new_root)

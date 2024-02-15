8eb6a1f24256642b556333cb0d75fe4834dc199a
Seth Daniel <seth@sethdaniel.org>
2024-02-15 19:47:07 +0000

    .

--- .config/nvim/lua/plugins/telescope-nvim.lua
+++ .config/nvim/lua/plugins/telescope-nvim.lua
@@ -38,102 +38,107 @@ vim.keymap.set('n', '<leader>pp', function()
   require('telescope').extensions.projects.projects {}
 end, {})
 
-require('telescope').setup {
-  defaults = {
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-    },
-    prompt_prefix = '   ',
-    selection_caret = '  ',
-    entry_prefix = '  ',
-    initial_mode = 'insert',
-    selection_strategy = 'reset',
-    sorting_strategy = 'descending',
-    layout_strategy = 'vertical',
-    file_sorter = require('telescope.sorters').get_fuzzy_file,
-    file_ignore_patterns = {},
-    generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
-    path_display = { 'truncate' },
-    winblend = 0,
-    layout_config = {
-      horizontal = { mirror = false },
-      vertical = { mirror = false },
-      prompt_position = 'bottom',
-      width = 0.90,
-      -- preview_cutoff = 120,
-    },
-    -- results_height = 1,
-    border = {},
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-    color_devicons = true,
-    use_less = true,
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = function(opts)
-      return require('telescope.previewers').vim_buffer_cat.new(opts)
-    end,
-    grep_previewer = function(opts)
-      return require('telescope.previewers').vim_buffer_vimgrep.new(opts)
-    end,
-    qflist_previewer = function(opts)
-      return require('telescope.previewers').vim_buffer_qflist.new(opts)
-    end,
-
-    -- Developer configurations: Not meant for general override
-    -- buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-  },
-  pickers = {
-    git_branches = {
-      mappings = {
-        i = {
-          ['<cr>'] = require('telescope.actions').git_switch_branch,
+vim.api.nvim_create_autocmd('VimEnter', {
+  once = true,
+  callback = function()
+    require('telescope').setup {
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
         },
-        n = {
-          ['<cr>'] = require('telescope.actions').git_switch_branch,
+        prompt_prefix = '   ',
+        selection_caret = '  ',
+        entry_prefix = '  ',
+        initial_mode = 'insert',
+        selection_strategy = 'reset',
+        sorting_strategy = 'descending',
+        layout_strategy = 'vertical',
+        file_sorter = require('telescope.sorters').get_fuzzy_file,
+        file_ignore_patterns = {},
+        generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
+        path_display = { 'truncate' },
+        winblend = 0,
+        layout_config = {
+          horizontal = { mirror = false },
+          vertical = { mirror = false },
+          prompt_position = 'bottom',
+          width = 0.90,
+          -- preview_cutoff = 120,
+        },
+        -- results_height = 1,
+        border = {},
+        borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+        color_devicons = true,
+        use_less = true,
+        set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+        file_previewer = function(opts)
+          return require('telescope.previewers').vim_buffer_cat.new(opts)
+        end,
+        grep_previewer = function(opts)
+          return require('telescope.previewers').vim_buffer_vimgrep.new(opts)
+        end,
+        qflist_previewer = function(opts)
+          return require('telescope.previewers').vim_buffer_qflist.new(opts)
+        end,
+
+        -- Developer configurations: Not meant for general override
+        -- buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+      },
+      pickers = {
+        git_branches = {
+          mappings = {
+            i = {
+              ['<cr>'] = require('telescope.actions').git_switch_branch,
+            },
+            n = {
+              ['<cr>'] = require('telescope.actions').git_switch_branch,
+            },
+          },
+        },
+        buffers = {
+          sort_lastused = true,
+          mappings = {
+            i = {
+              ['<c-d>'] = require('telescope.actions').delete_buffer,
+            },
+            n = {
+              ['<c-d>'] = require('telescope.actions').delete_buffer,
+            },
+          },
         },
       },
-    },
-    buffers = {
-      sort_lastused = true,
-      mappings = {
-        i = {
-          ['<c-d>'] = require('telescope.actions').delete_buffer,
-        },
-        n = {
-          ['<c-d>'] = require('telescope.actions').delete_buffer,
-        },
+      extensions = {
+        fzy_native = { override_generic_sorter = true, override_file_sorter = true },
+        -- live_grep_args = {},
       },
-    },
-  },
-  extensions = {
-    fzy_native = { override_generic_sorter = true, override_file_sorter = true },
-    -- live_grep_args = {},
-  },
-}
+    }
 
-local ok, _ = pcall(require, 'goldsmith')
-if ok then
-  require('telescope').load_extension 'goldsmith'
-end
-local ok, _ = pcall(require, 'repossession')
-if ok then
-  require('telescope').load_extension 'repossession'
-end
-local ok, _ = pcall(require, 'goofball')
-if ok then
-  require('telescope').load_extension 'goofball'
-end
-local ok, _ = pcall(require, 'projects')
-if ok then
-  require('telescope').load_extension 'projects'
-end
-local ok, _ = pcall(require, 'possession')
-if ok then
-  require('telescope').load_extension 'possession'
-end
-require('telescope').load_extension 'fzy_native'
+    local ok, _ = pcall(require, 'goldsmith')
+    if ok then
+      require('telescope').load_extension 'goldsmith'
+    end
+    local ok, _ = pcall(require, 'repossession')
+    if ok then
+      require('telescope').load_extension 'repossession'
+    end
+    local ok, _ = pcall(require, 'goofball')
+    if ok then
+      require('telescope').load_extension 'goofball'
+    end
+    local ok, _ = pcall(require, 'projects')
+    if ok then
+      require('telescope').load_extension 'projects'
+    end
+    local ok, _ = pcall(require, 'possession')
+    if ok then
+      require('telescope').load_extension 'possession'
+    end
+    require('telescope').load_extension 'fzy_native'
+  end,
+})

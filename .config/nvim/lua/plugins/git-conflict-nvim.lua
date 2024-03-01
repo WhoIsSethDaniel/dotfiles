141cc1178db29d9bd11fa732ad451b618cdb653f
Seth Daniel <seth@sethdaniel.org>
2024-03-01 16:54:19 -0600

    .

--- .config/nvim/lua/plugins/git-conflict-nvim.lua
+++ .config/nvim/lua/plugins/git-conflict-nvim.lua
@@ -11,10 +11,10 @@ require('git-conflict').setup {
   debug = false,
 }
 
+-- these are all the default maps
 vim.keymap.set('n', 'co', '<Plug>(git-conflict-ours)')
 vim.keymap.set('n', 'ct', '<Plug>(git-conflict-theirs)')
 vim.keymap.set('n', 'cb', '<Plug>(git-conflict-both)')
 vim.keymap.set('n', 'c0', '<Plug>(git-conflict-none)')
--- try [n & ]n instead -- part of unimpaired
--- vim.keymap.set('n', ']x', '<Plug>(git-conflict-prev-conflict)')
--- vim.keymap.set('n', '[x', '<Plug>(git-conflict-next-conflict)')
+vim.keymap.set('n', ']x', '<Plug>(git-conflict-prev-conflict)')
+vim.keymap.set('n', '[x', '<Plug>(git-conflict-next-conflict)')

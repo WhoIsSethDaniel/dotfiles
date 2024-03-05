f3a0548fd2850cbb6916075488590a2b5e46973f
Seth Daniel <seth@sethdaniel.org>
2024-03-05 13:23:09 -0600

    .

--- .config/nvim/lua/plugins/lualine-nvim.lua
+++ .config/nvim/lua/plugins/lualine-nvim.lua
@@ -43,9 +43,6 @@ require('lualine').setup {
     },
     lualine_x = {
       { "require'goldsmith'.status()" },
-      -- { 'lsp_progress' },
-      -- { 'lsp_progress', only_show_attached = true, hide = { 'null-ls' } },
-      -- { 'lsp_progress', hide = { 'null-ls' } },
       'encoding',
       'fileformat',
       'filetype',

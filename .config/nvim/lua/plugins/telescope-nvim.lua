cb291c363a503ac5ca9129504695dc7ec0a80138
Seth Daniel <seth@sethdaniel.org>
2024-03-05 13:51:16 -0600

    .

--- .config/nvim/lua/plugins/telescope-nvim.lua
+++ .config/nvim/lua/plugins/telescope-nvim.lua
@@ -114,8 +114,14 @@ vim.api.nvim_create_autocmd('VimEnter', {
         },
       },
       extensions = {
-        fzy_native = { override_generic_sorter = true, override_file_sorter = true },
+        -- fzy_native = { override_generic_sorter = true, override_file_sorter = true },
         -- live_grep_args = {},
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = 'smart_case', -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
+        },
         ['ui-select'] = {
           require('telescope.themes').get_dropdown {},
         },
@@ -134,7 +140,8 @@ vim.api.nvim_create_autocmd('VimEnter', {
     if ok then
       require('telescope').load_extension 'projects'
     end
-    require('telescope').load_extension 'fzy_native'
+    -- require('telescope').load_extension 'fzy_native'
+    require('telescope').load_extension 'fzf'
     require('telescope').load_extension 'ui-select'
   end,
 })

c36ab61792163b6aa84801b386d35d5199f0907f
Seth Daniel <seth@sethdaniel.org>
2024-03-05 13:16:18 -0600

    .

--- .config/nvim/lua/plugins/lsp/lua_ls.lua
+++ .config/nvim/lua/plugins/lsp/lua_ls.lua
@@ -1,6 +1,32 @@
 -- https://github.com/LuaLS/lua-language-server
 -- https://github.com/LuaLS/lua-language-server/wiki/Settings
 return {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        },
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      },
+    })
+  end,
   settings = {
     Lua = {
       format = {
@@ -8,7 +34,7 @@ return {
         enable = false,
       },
       runtime = {
-        version = 'LuaJIT',
+        -- version = 'LuaJIT',
       },
       hint = {
         enable = true,
@@ -19,14 +45,16 @@ return {
         paramName = 'Disable',
       },
       workspace = {
-        library = {
-          vim.env.VIMRUNTIME,
-        },
-        checkThirdParty = false,
+        -- library = {
+        --   vim.env.VIMRUNTIME,
+        -- },
+        -- checkThirdParty = false,
         -- maxPreload = 4000,
         -- preloadFileSize = 150,
       },
-      telemetry = { enable = false },
+      telemetry = {
+        enable = false,
+      },
     },
   },
 }

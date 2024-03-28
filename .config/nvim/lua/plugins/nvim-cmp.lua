2658d523f3fd7cab75df19c9a2f466c3864293ba
Seth Daniel <seth@sethdaniel.org>
2024-03-28 13:54:42 -0500

    .

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -9,27 +9,20 @@ local luasnip = require 'luasnip'
 luasnip.config.setup {}
 cmp.setup {
   formatting = {
-    format = function(entry, vim_item)
-      vim_item = require('lspkind').cmp_format()(entry, vim_item)
-      local alias = {
-        buffer = 'buffer',
-        path = 'path',
-        calc = 'calc',
-        emoji = 'emoji',
-        nvim_lsp = 'LSP',
-        luasnip = 'luasnip',
-        vsnip = 'vsnip',
-        nvim_lua = 'lua',
-        nvim_lsp_signature_help = 'LSP Signature',
-      }
+    format = require('lspkind').cmp_format {
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      -- can also be a function to dynamically calculate max width such as
+      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
-      if entry.source.name == 'nvim_lsp' then
-        vim_item.menu = entry.source.source.client.name
-      else
-        vim_item.menu = alias[entry.source.name] or entry.source.name
-      end
-      return vim_item
-    end,
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      -- before = function(entry, vim_item)
+      --   return vim_item
+      -- end,
+    },
   },
   window = {
     completion = cmp.config.window.bordered { border = 'rounded' },

1bb105542f4d71d60f20ff41a1a9eacc010861c1
Seth Daniel <seth@sethdaniel.org>
2024-03-29 14:10:36 -0500

    .

---
+++ .config/nvim/lua/plugins/nvim-spider.lua
@@ -0,0 +1,10 @@
+-- https://github.com/chrisgrieser/nvim-spider
+require('spider').setup {
+  skipInsignificantPunctuation = true,
+  subwordMovement = true,
+  customPatterns = {}, -- check "Custom Movement Patterns" in the README for details
+}
+
+vim.keymap.set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>", { desc = 'Spider-w' })
+vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>", { desc = 'Spider-e' })
+vim.keymap.set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>", { desc = 'Spider-b' })

5a561d3b04befe296e9cbaf3732c61fe857a9753
Seth Daniel <seth@sethdaniel.org>
2024-04-01 16:55:30 -0500

    .

--- .config/nvim/lua/plugins/gitsigns-nvim.lua
+++ .config/nvim/lua/plugins/gitsigns-nvim.lua
@@ -1,10 +1,11 @@
 require('gitsigns').setup {
   signs = {
-    add = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-    change = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    delete = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    topdelete = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    add = { text = '│' },
+    change = { text = '│' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked = { text = '┆' },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -14,6 +15,7 @@ require('gitsigns').setup {
     interval = 1000,
     follow_files = true,
   },
+  auto_attach = true,
   attach_to_untracked = true,
   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
@@ -21,7 +23,9 @@ require('gitsigns').setup {
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
     ignore_whitespace = false,
+    virt_text_priority = 100,
   },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
   current_line_blame_formatter_opts = {
     relative_time = false,
   },

2e454dee32e67750ba171ec8704aa601731296f6
Seth Daniel <seth@sethdaniel.org>
2024-06-01 16:40:56 -0500

    .

--- .config/nvim/lua/plugins/toggleterm-nvim.lua
+++ .config/nvim/lua/plugins/toggleterm-nvim.lua
@@ -37,4 +37,4 @@ require('toggleterm').setup {
 -- fix <s-space> problem (for now)
 -- https://www.reddit.com/r/neovim/comments/1340ttr/issue_with_terminal_and_recent_wezterm/
 -- https://github.com/vim/vim/issues/6040
-vim.api.nvim_set_keymap('t', '<s-space>', '<space>', {})
+vim.keymap.set('t', '<s-space>', '<space>', { noremap = true, silent = true, desc = 'toggleterm wezterm fix' })

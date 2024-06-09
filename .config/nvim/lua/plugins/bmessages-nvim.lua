d30c0f7f3c5da1792597cd541feb0b02ec22193a
Seth Daniel <seth@sethdaniel.org>
2024-06-08 21:29:58 -0500

    .

--- .config/nvim/lua/plugins/bmessages-nvim.lua
+++ .config/nvim/lua/plugins/bmessages-nvim.lua
@@ -1,2 +1,3 @@
+-- https://github.com/ariel-frischer/bmessages.nvim
 require('bmessages').setup()
 vim.api.nvim_set_keymap('n', '<leader>mm', '<cmd>Bmessages<cr><c-w>p', { noremap = true, silent = true })

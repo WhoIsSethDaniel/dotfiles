b3bc99452af9fa7161b72514f0f5897a9b12a7e7
Seth Daniel <seth@sethdaniel.org>
2024-07-18 11:07:34 -0500

    .

--- .config/nvim/lua/plugins/project-nvim.lua
+++ .config/nvim/lua/plugins/project-nvim.lua
@@ -1,6 +1,6 @@
 require('project_nvim').setup {
   detection_methods = { 'lsp', 'pattern' },
-  patterns = { 'go.work', '.git', 'go.mod', 'selene.toml', 'stylua.toml' },
+  patterns = { '.git', 'go.work', 'go.mod', 'selene.toml', 'stylua.toml' },
   silent_chdir = true,
   ignore_lsp = {},
   scope_chdir = 'global',

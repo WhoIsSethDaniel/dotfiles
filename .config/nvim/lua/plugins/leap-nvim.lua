3751ba98fe44af32d3506425f84dbce2e0f18622
Seth Daniel <seth@sethdaniel.org>
2024-07-22 13:40:38 -0500

    .

--- .config/nvim/lua/plugins/leap-nvim.lua
+++ .config/nvim/lua/plugins/leap-nvim.lua
@@ -1,1 +1,4 @@
-require('leap').create_default_mappings()
+vim.keymap.set('n', 'gs', '<Plug>(leap)')
+vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+vim.keymap.set({ 'x', 'o' }, 'gs', '<Plug>(leap-forward)')
+vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')

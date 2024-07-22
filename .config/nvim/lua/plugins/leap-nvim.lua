403d7ca599ff61b14c265124a0119f060fe582a5
Seth Daniel <seth@sethdaniel.org>
2024-07-22 13:41:45 -0500

    .

--- .config/nvim/lua/plugins/leap-nvim.lua
+++ .config/nvim/lua/plugins/leap-nvim.lua
@@ -1,3 +1,5 @@
+-- https://github.com/ggandor/leap.nvim
+-- change 's' default to 'gs' so as not to conflict with vim-sandwich
 vim.keymap.set('n', 'gs', '<Plug>(leap)')
 vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
 vim.keymap.set({ 'x', 'o' }, 'gs', '<Plug>(leap-forward)')

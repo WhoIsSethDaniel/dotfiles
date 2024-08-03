15b85394c4974021aa5658042a46d1a62ee4d8fb
Seth Daniel <seth@sethdaniel.org>
2024-08-03 16:41:36 -0500

    .

--- .config/nvim/lua/plugins/tokyonight-nvim.lua
+++ .config/nvim/lua/plugins/tokyonight-nvim.lua
@@ -1,3 +1,4 @@
+-- https://github.com/folke/tokyonight.nvim
 require('tokyonight').setup {
   -- your configuration comes here
   -- or leave it empty to use the default settings
@@ -24,7 +25,6 @@ require('tokyonight').setup {
 
   --- You can override specific color groups to use other groups or a hex color
   --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
   on_colors = function(c)
     c.black = '#000000'
     c.bg = '#000000'
@@ -36,9 +36,7 @@ require('tokyonight').setup {
 
   --- You can override specific highlights to use other groups or a hex color
   --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors)
+  on_highlights = function(highlights, _)
     highlights.Normal = {
       bg = '#000000',
     }

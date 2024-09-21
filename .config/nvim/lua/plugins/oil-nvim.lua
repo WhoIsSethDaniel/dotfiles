87c6937f56ae75d05388f2ff663e458fe224db51
Seth Daniel <seth@sethdaniel.org>
2024-09-21 14:19:20 -0500

    .

--- .config/nvim/lua/plugins/oil-nvim.lua
+++ .config/nvim/lua/plugins/oil-nvim.lua
@@ -56,6 +56,25 @@ oil.setup {
   view_options = {
     -- Show files and directories that start with "."
     show_hidden = false,
+    -- This function defines what is considered a "hidden" file
+    is_hidden_file = function(name, bufnr)
+      return vim.startswith(name, '.')
+    end,
+    -- This function defines what will never be shown, even when `show_hidden` is set
+    is_always_hidden = function(name, bufnr)
+      return vim.startswith(name, '..')
+    end,
+    -- Sort file names in a more intuitive order for humans. Is less performant,
+    -- so you may want to set to false if you work with large directories.
+    natural_order = true,
+    -- Sort file and directory names case insensitive
+    case_insensitive = false,
+    sort = {
+      -- sort order can be "asc" or "desc"
+      -- see :help oil-columns to see which columns are sortable
+      { 'type', 'asc' },
+      { 'name', 'asc' },
+    },
   },
   -- Configuration for the floating window in oil.open_float
   float = {

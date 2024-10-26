ed67828356707117f5d604fe5a76bebb311f0d6c
Seth Daniel <seth@sethdaniel.org>
2024-10-26 17:04:43 -0500

    .

--- .config/nvim/lua/plugins/magazine-nvim.lua
+++ .config/nvim/lua/plugins/magazine-nvim.lua
@@ -16,20 +16,26 @@ cmp.setup {
     completeopt = 'menu,menuone',
   },
   formatting = {
-    format = require('lspkind').cmp_format {
-      mode = 'symbol_text', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      -- can also be a function to dynamically calculate max width such as
-      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+    format = function()
+      local ok, lk = pcall(require, 'lspkind')
+      if not ok then
+        return
+      end
+      lk.cmp_format {
+        mode = 'symbol_text', -- show only symbol annotations
+        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      -- before = function(entry, vim_item)
-      --   return vim_item
-      -- end,
-    },
+        -- The function below will be called before any actual modifications from lspkind
+        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+        -- before = function(entry, vim_item)
+        --   return vim_item
+        -- end,
+      }
+    end,
   },
   window = {
     completion = cmp.config.window.bordered { border = 'rounded' },

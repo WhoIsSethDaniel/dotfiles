60ed48324189b31c372889948ff5b7316cdf33f1
Seth Daniel <seth@sethdaniel.org>
2025-02-23 14:30:28 -0600

    .

--- .config/nvim/lua/plugins/workspaces-nvim.lua
+++ .config/nvim/lua/plugins/workspaces-nvim.lua
@@ -22,7 +22,7 @@ require('workspaces').setup {
   mru_sort = true,
 
   -- option to automatically activate workspace when opening neovim in a workspace directory
-  auto_open = false,
+  auto_open = true,
 
   -- option to automatically activate workspace when changing directory not via this plugin
   -- set to "autochdir" to enable auto_dir when using :e and vim.opt.autochdir

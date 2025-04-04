d0966537b10066e71341d51225497b36266d00d1
Seth Daniel <seth@sethdaniel.org>
2025-04-04 15:24:25 -0500

    .

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -25,6 +25,28 @@ require('blink.cmp').setup {
     ghost_text = {
       enabled = true,
     },
+    menu = {
+      draw = {
+        components = {
+          kind_icon = {
+            text = function(ctx)
+              local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+              return kind_icon
+            end,
+            highlight = function(ctx)
+              local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+              return hl
+            end,
+          },
+          kind = {
+            highlight = function(ctx)
+              local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+              return hl
+            end,
+          },
+        },
+      },
+    },
   },
   -- https://cmp.saghen.dev/modes/cmdline.html
   cmdline = {

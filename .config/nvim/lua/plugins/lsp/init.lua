5acf855efb06f4f80f750e2be30ca496d1ffdaaf
Seth Daniel <seth@sethdaniel.org>
2025-04-11 16:18:21 -0500

    .

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -53,7 +53,7 @@ vim.api.nvim_create_autocmd({ 'LspAttach' }, {
     local bufnr = args.buf
     local client_id = args.data.client_id
     local client = vim.lsp.get_client_by_id(client_id)
-    -- notify(client.name .. ' (attached)')
+    notify(client.name .. ' (attached)')
 
     -- prefix diagnostics with the name of the client
     local ns = vim.lsp.diagnostic.get_namespace(client_id)

98e89732dcf369451f02efebb8393c99353f5157
Seth Daniel <seth@sethdaniel.org>
2025-04-14 18:51:00 +0000

    .

--- .config/nvim/lua/plugins/lsp/lua_ls.lua
+++ .config/nvim/lua/plugins/lsp/lua_ls.lua
@@ -2,10 +2,10 @@
 -- https://luals.github.io/wiki/settings/
 return {
   on_init = function(client)
-    local path = client.workspace_folders and client.workspace_folders[1].name
-    if path and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
-      return
-    end
+    -- local path = client.workspace_folders and client.workspace_folders[1].name
+    -- if path and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+    --   return
+    -- end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
       runtime = {

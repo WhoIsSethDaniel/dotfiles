2aa8204af56bcc300a1e941cd2f9b0a624deeb0f
Seth Daniel <seth@sethdaniel.org>
2025-04-17 14:49:03 -0500

    .

--- .config/nvim/lua/plugins/hardtime-nvim.lua
+++ .config/nvim/lua/plugins/hardtime-nvim.lua
@@ -1,7 +1,7 @@
 -- https://github.com/m4xshen/hardtime.nvim
 local config = require('hardtime.config').config
 config['restricted_keys']['-'] = nil
-vim.list_extend(config.disabled_filetypes, { 'wiki', 'godoc', 'man', 'outputpanel' })
+vim.list_extend(config.disabled_filetypes, { 'wiki', 'godoc', 'man', 'outputpanel', 'json' })
 require('hardtime').setup {
   max_count = 4,
 }

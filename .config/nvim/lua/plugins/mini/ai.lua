fcbad45eee50cccef6f1031da4a142384e87b714
Seth Daniel <seth@sethdaniel.org>
2025-05-20 14:06:38 -0500

    .

--- .config/nvim/lua/plugins/mini/ai.lua
+++ .config/nvim/lua/plugins/mini/ai.lua
@@ -6,32 +6,4 @@ require('mini.ai').setup {
   custom_textobjects = {
     F = spec_treesitter { a = '@function.outer', i = '@function.inner' },
   },
-
-  -- Module mappings. Use `''` (empty string) to disable one.
-  mappings = {
-    -- Main textobject prefixes
-    around = 'a',
-    inside = 'i',
-
-    -- Next/last variants
-    around_next = 'an',
-    inside_next = 'in',
-    around_last = 'al',
-    inside_last = 'il',
-
-    -- Move cursor to corresponding edge of `a` textobject
-    goto_left = 'g[',
-    goto_right = 'g]',
-  },
-
-  -- Number of lines within which textobject is searched
-  n_lines = 50,
-
-  -- How to search for object (first inside current line, then inside
-  -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-  -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
-  search_method = 'cover_or_next',
-
-  -- Whether to disable showing non-error feedback
-  silent = false,
 }

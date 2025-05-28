98136a5e96df9f415180ddda17394d174ca7fe2f
Seth Daniel <seth@sethdaniel.org>
2025-05-28 17:04:03 -0500

    .

---
+++ .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -0,0 +1,19 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/tree/main
+-- require('nvim-treesitter-textobjects').setup {
+--   move = {
+--     set_jumps = true,
+--   },
+-- }
+--
+-- vim.keymap.set({ 'n', 'x', 'o' }, ']]', function()
+--   require('nvim-treesitter-textobjects.move').goto_next_start('@function.outer', 'textobjects')
+-- end)
+-- vim.keymap.set({ 'n', 'x', 'o' }, '][', function()
+--   require('nvim-treesitter-textobjects.move').goto_next_end('@function.outer', 'textobjects')
+-- end)
+-- vim.keymap.set({ 'n', 'x', 'o' }, '[[', function()
+--   require('nvim-treesitter-textobjects.move').goto_previous_start('@function.outer', 'textobjects')
+-- end)
+-- vim.keymap.set({ 'n', 'x', 'o' }, '[]', function()
+--   require('nvim-treesitter-textobjects.move').goto_previous_end('@function.outer', 'textobjects')
+-- end)
--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,58 +1,65 @@
---  https://github.com/nvim-treesitter/nvim-treesitter
-vim.api.nvim_create_autocmd('BufRead', {
-  once = true,
-  callback = function()
-    ---@diagnostic disable-next-line:missing-fields
-    require('nvim-treesitter.configs').setup {
-      auto_install = true,
-      -- the comment parser is for comment *tags* such as TODO and FIXME;
-      -- see https://github.com/stsewd/tree-sitter-comment
-      ensure_installed = {
-        'comment',
-        'diff',
-        'luadoc',
-        'pod',
-        'regex',
-        'sql',
+--  https://github.com/nvim-treesitter/nvim-treesitter/tree/main
+---@diagnostic disable-next-line:missing-fields
+
+-- main branch treesitter
+-- require('nvim-treesitter').install {
+--   'comment',
+--   'diff',
+--   'luadoc',
+--   'pod',
+--   'regex',
+--   'sql',
+-- }
+
+-- master branch treesitter
+require('nvim-treesitter.configs').setup {
+  auto_install = true,
+  -- the comment parser is for comment *tags* such as TODO and FIXME;
+  -- see https://github.com/stsewd/tree-sitter-comment
+  ensure_installed = {
+    'comment',
+    'diff',
+    'luadoc',
+    'pod',
+    'regex',
+    'sql',
+  },
+  -- ignore_install = { 'cooklang' },
+  autopairs = {
+    enable = true,
+  },
+  endwise = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+  matchup = {
+    enable = true,
+    enable_quotes = true,
+  },
+  textobjects = {
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']]'] = '@function.outer',
       },
-      -- ignore_install = { 'cooklang' },
-      autopairs = {
-        enable = true,
+      goto_next_end = {
+        [']['] = '@function.outer',
       },
-      endwise = {
-        enable = true,
+      goto_previous_start = {
+        ['[['] = '@function.outer',
       },
-      highlight = {
-        enable = true,
+      goto_previous_end = {
+        ['[]'] = '@function.outer',
       },
-      incremental_selection = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-        enable_quotes = true,
-      },
-      textobjects = {
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']]'] = '@function.outer',
-          },
-          goto_next_end = {
-            [']['] = '@function.outer',
-          },
-          goto_previous_start = {
-            ['[['] = '@function.outer',
-          },
-          goto_previous_end = {
-            ['[]'] = '@function.outer',
-          },
-        },
-      },
-    }
-  end,
-})
+    },
+  },
+}

210de8a33ebdd4d6eff52e059cbcdd49233cbcc7
Seth Daniel <seth@sethdaniel.org>
2025-05-28 22:04:08 -0500

    .

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,65 +1,76 @@
 --  https://github.com/nvim-treesitter/nvim-treesitter/tree/main
----@diagnostic disable-next-line:missing-fields
+local masterts, _ = pcall(require, 'nvim-treesitter.configs')
 
 -- main branch treesitter
--- require('nvim-treesitter').install {
---   'comment',
---   'diff',
---   'luadoc',
---   'pod',
---   'regex',
---   'sql',
--- }
-
--- master branch treesitter
-require('nvim-treesitter.configs').setup {
-  auto_install = true,
-  -- the comment parser is for comment *tags* such as TODO and FIXME;
-  -- see https://github.com/stsewd/tree-sitter-comment
-  ensure_installed = {
+if not masterts then
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'perl', 'go', 'markdown' },
+    callback = function()
+      vim.treesitter.start()
+    end,
+  })
+  require('nvim-treesitter').install {
     'comment',
     'diff',
     'luadoc',
     'pod',
     'regex',
     'sql',
-  },
-  -- ignore_install = { 'cooklang' },
-  autopairs = {
-    enable = true,
-  },
-  endwise = {
-    enable = true,
-  },
-  highlight = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-  matchup = {
-    enable = true,
-    enable_quotes = true,
-  },
-  textobjects = {
-    move = {
+  }
+  -- for now: everything
+  vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+else
+  -- master branch treesitter
+  ---@diagnostic disable-next-line:missing-fields
+  require('nvim-treesitter.configs').setup {
+    auto_install = true,
+    -- the comment parser is for comment *tags* such as TODO and FIXME;
+    -- see https://github.com/stsewd/tree-sitter-comment
+    ensure_installed = {
+      'comment',
+      'diff',
+      'luadoc',
+      'pod',
+      'regex',
+      'sql',
+    },
+    -- ignore_install = { 'cooklang' },
+    autopairs = {
       enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        [']]'] = '@function.outer',
-      },
-      goto_next_end = {
-        [']['] = '@function.outer',
-      },
-      goto_previous_start = {
-        ['[['] = '@function.outer',
-      },
-      goto_previous_end = {
-        ['[]'] = '@function.outer',
+    },
+    endwise = {
+      enable = true,
+    },
+    highlight = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    matchup = {
+      enable = true,
+      enable_quotes = true,
+    },
+    textobjects = {
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          [']]'] = '@function.outer',
+        },
+        goto_next_end = {
+          [']['] = '@function.outer',
+        },
+        goto_previous_start = {
+          ['[['] = '@function.outer',
+        },
+        goto_previous_end = {
+          ['[]'] = '@function.outer',
+        },
       },
     },
-  },
-}
+  }
+end

f4ff0841b2cd4d58105f496755d8ab42b58b4426
Seth Daniel <seth@sethdaniel.org>
2025-07-30 14:47:22 -0500

    .

--- .bash-init/work.sh
+++ .bash-init/work.sh
@@ -3,3 +3,6 @@
 if [[ -f ~/.config/dotfiles/main/bash/work.sh ]]; then
     . ~/.config/dotfiles/main/bash/work.sh
 fi
+if [[ -d ~/.config/dotfiles/main/bin ]]; then
+    set_post_path_var PATH ~/.config/dotfiles/main/bin
+fi

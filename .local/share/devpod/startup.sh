2d9a2abb61390296304ec124fd5b0180f94d3eae
Seth Daniel <seth@sethdaniel.org>
2025-11-14 13:33:54 -0600

    .

--- .local/share/devpod/startup.sh
+++ .local/share/devpod/startup.sh
@@ -4,3 +4,5 @@ echo "RUNNING devpod state sync"
 scp "$HOME"/.inputrc mmwebsite:.
 scp "$HOME"/.psqlrc mmwebsite:.
 scp "$HOME"/.ripgreprc mmwebsite:.
+cp-file.sh /usr/local/share/GeoIP/GeoIP2-City.mmdb
+cp-file.sh /usr/local/share/GeoIP/GeoIP2-ISP.mmdb

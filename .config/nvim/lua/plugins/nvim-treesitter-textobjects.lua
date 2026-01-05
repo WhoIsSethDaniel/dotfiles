08d2b109313cb47d48a6db220ab5f7cd90da0510
Seth Daniel <seth@sethdaniel.org>
2026-01-05 17:16:32 -0600

    .

--- .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,28 +1,25 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/tree/main
-local masterts, _ = pcall(require, 'nvim-treesitter.configs')
-if not masterts then
-  require('nvim-treesitter-textobjects').setup {
-    move = {
-      set_jumps = true,
-    },
-  }
+require('nvim-treesitter-textobjects').setup {
+  move = {
+    set_jumps = true,
+  },
+}
 
-  vim.keymap.set({ 'n', 'x', 'o' }, ']]', function()
-    require('nvim-treesitter-textobjects.move').goto_next_start('@function.outer', 'textobjects')
-  end)
-  vim.keymap.set({ 'n', 'x', 'o' }, '][', function()
-    require('nvim-treesitter-textobjects.move').goto_next_end('@function.outer', 'textobjects')
-  end)
-  vim.keymap.set({ 'n', 'x', 'o' }, '[[', function()
-    require('nvim-treesitter-textobjects.move').goto_previous_start('@function.outer', 'textobjects')
-  end)
-  vim.keymap.set({ 'n', 'x', 'o' }, '[]', function()
-    require('nvim-treesitter-textobjects.move').goto_previous_end('@function.outer', 'textobjects')
-  end)
-  vim.keymap.set({ 'x', 'o' }, 'af', function()
-    require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects')
-  end)
-  vim.keymap.set({ 'x', 'o' }, 'if', function()
-    require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects')
-  end)
-end
+vim.keymap.set({ 'n', 'x', 'o' }, ']]', function()
+  require('nvim-treesitter-textobjects.move').goto_next_start('@function.outer', 'textobjects')
+end)
+vim.keymap.set({ 'n', 'x', 'o' }, '][', function()
+  require('nvim-treesitter-textobjects.move').goto_next_end('@function.outer', 'textobjects')
+end)
+vim.keymap.set({ 'n', 'x', 'o' }, '[[', function()
+  require('nvim-treesitter-textobjects.move').goto_previous_start('@function.outer', 'textobjects')
+end)
+vim.keymap.set({ 'n', 'x', 'o' }, '[]', function()
+  require('nvim-treesitter-textobjects.move').goto_previous_end('@function.outer', 'textobjects')
+end)
+vim.keymap.set({ 'x', 'o' }, 'af', function()
+  require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects')
+end)
+vim.keymap.set({ 'x', 'o' }, 'if', function()
+  require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects')
+end)
--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,6 @@
 --  https://github.com/nvim-treesitter/nvim-treesitter/tree/main
 --  https://github.com/nvim-treesitter/nvim-treesitter-textobjects/tree/main
 --  https://www.reddit.com/r/neovim/comments/1pndf9e/my_new_nvimtreesitter_configuration_for_the_main/
-local masterts, _ = pcall(require, 'nvim-treesitter.configs')
-
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
     vim.cmd.TSUpdate()
@@ -18,80 +16,30 @@ local auto_install = {
   'sql',
 }
 
--- main branch treesitter
-if not masterts then
-  local ts = require 'nvim-treesitter'
+local ts = require 'nvim-treesitter'
 
-  ts.install(auto_install)
+ts.install(auto_install)
 
-  local ignore_filetypes = {}
+local ignore_filetypes = {}
 
-  -- Auto-install parsers and enable highlighting on FileType
-  vim.api.nvim_create_autocmd('FileType', {
-    group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true }),
-    desc = 'Enable treesitter highlighting and indentation',
-    callback = function(event)
-      if vim.tbl_contains(ignore_filetypes, event.match) then
-        return
+-- Auto-install parsers and enable highlighting on FileType
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true }),
+  desc = 'Enable treesitter highlighting and indentation',
+  callback = function(event)
+    if vim.tbl_contains(ignore_filetypes, event.match) then
+      return
+    end
+
+    local lang = vim.treesitter.language.get_lang(event.match) or event.match
+    local buf = event.buf
+
+    -- Install missing parsers (no-op if already installed)
+    ts.install(lang):await(function()
+      if vim.treesitter.language.add(lang) then
+        vim.treesitter.start(buf, lang)
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end
-
-      local lang = vim.treesitter.language.get_lang(event.match) or event.match
-      local buf = event.buf
-
-      -- Install missing parsers (no-op if already installed)
-      ts.install(lang):await(function()
-        if vim.treesitter.language.add(lang) then
-          vim.treesitter.start(buf, lang)
-          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
-      end)
-    end,
-  })
-else
-  -- master branch treesitter
-  ---@diagnostic disable-next-line:missing-fields
-  require('nvim-treesitter.configs').setup {
-    auto_install = true,
-    -- the comment parser is for comment *tags* such as TODO and FIXME;
-    -- see https://github.com/stsewd/tree-sitter-comment
-    ensure_installed = auto_install,
-    -- ignore_install = { 'cooklang' },
-    autopairs = {
-      enable = true,
-    },
-    endwise = {
-      enable = true,
-    },
-    highlight = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-    matchup = {
-      enable = true,
-      enable_quotes = true,
-    },
-    textobjects = {
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          [']]'] = '@function.outer',
-        },
-        goto_next_end = {
-          [']['] = '@function.outer',
-        },
-        goto_previous_start = {
-          ['[['] = '@function.outer',
-        },
-        goto_previous_end = {
-          ['[]'] = '@function.outer',
-        },
-      },
-    },
-  }
-end
+    end)
+  end,
+})

0bf29d578cacc8b920a76dfe9ba4824a2bdff651
Seth Daniel <seth@sethdaniel.org>
2026-08-26 14:03:24 -0500

    .

--- .bash-init/01-mise.sh
+++ .bash-init/01-mise.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [[ -f ~/.config/dotfiles/main/.config/mise/mise.work.toml ]]; then
+    ln -sf ~/.config/dotfiles/main/.config/mise/mise.work.toml ~/.config/mise/mise.work.toml
+fi
+
 eval "$(mise activate bash)"
-
-

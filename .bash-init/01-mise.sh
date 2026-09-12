40b2fe465e6cd50574a1ca20b879185594d496f7
Seth Daniel <seth@sethdaniel.org>
2026-09-12 16:35:24 -0500

    .

--- .bash-init/01-mise.sh
+++ .bash-init/01-mise.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 WORK_MISE=~/.config/dotfiles/main/.config/mise/mise.work.toml
+HOME_MISE=~/.config/dotfiles/main/.config/mise/mise.home.toml
 if [[ -e $WORK_MISE ]]; then
     ln -sf "$WORK_MISE" ~/.config/mise/mise.work.toml
     set_export_var MISE_ENV work
+    eval "$(mise --env work activate bash)"
+elif [[ -e $HOME_MISE ]]; then
+    ln -sf "$WORK_MISE" ~/.config/mise/mise.home.toml
+    set_export_var MISE_ENV home
     eval "$(mise --env work activate bash)"
 else
     eval "$(mise activate bash)"
